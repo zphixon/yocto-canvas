d468824cc967217216832957867cb5948b79ffad
Zack <zphixon@gmail.com>
2021-04-02 18:25:39 -0400

    switch to minifb

--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,73 @@
-use miniquad::*;
+use minifb::{Key, ScaleMode, Window, WindowOptions};
 
-#[repr(C)]
-struct Vec2 {
-    x: f32,
-    y: f32,
-}
-
-#[repr(C)]
-struct Vertex {
-    pos: Vec2,
-    uv: Vec2,
-}
-
-struct Stage {
-    pipeline: Pipeline,
-    bindings: Bindings,
-}
-
-impl Stage {
-    fn new(ctx: &mut Context) -> Stage {
-        #[rustfmt::skip]
-        let vertices: [Vertex; 4] = [
-            Vertex { pos : Vec2 { x: -0.5, y: -0.5 }, uv: Vec2 { x: 0., y: 0. } },
-            Vertex { pos : Vec2 { x:  0.5, y: -0.5 }, uv: Vec2 { x: 1., y: 0. } },
-            Vertex { pos : Vec2 { x:  0.5, y:  0.5 }, uv: Vec2 { x: 1., y: 1. } },
-            Vertex { pos : Vec2 { x: -0.5, y:  0.5 }, uv: Vec2 { x: 0., y: 1. } },
-        ];
-
-        let vertex_buffer = Buffer::immutable(ctx, BufferType::VertexBuffer, &vertices);
-        let indices: [u16; 6] = [0, 1, 2, 0, 2, 3];
-        let index_buffer = Buffer::immutable(ctx, BufferType::IndexBuffer, &indices);
-
-        let pixels: [u8; 4 * 4 * 4] = [
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00,
-            0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF,
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF,
-            0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-            0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-        ];
-
-        let texture = Texture::from_rgba8(ctx, 4, 4, &pixels);
-
-        let bindings = Bindings {
-            vertex_buffers: vec![vertex_buffer],
-            index_buffer,
-            images: vec![texture],
-        };
-
-        let shader = Shader::new(ctx, shader::VERTEX, shader::FRAGMENT, shader::meta()).unwrap();
-
-        let pipeline = Pipeline::new(
-            ctx,
-            &[BufferLayout::default()],
-            &[
-                VertexAttribute::new("pos", VertexFormat::Float2),
-                VertexAttribute::new("uv", VertexFormat::Float2),
-            ],
-            shader,
-        );
-
-        Stage { pipeline, bindings }
-    }
-}
-
-impl EventHandler for Stage {
-    fn update(&mut self, ctx: &mut Context) {}
-
-    fn draw(&mut self, ctx: &mut Context) {
-        let t = std::time::Instant::now();
-        ctx.begin_default_pass(Default::default());
-        ctx.apply_pipeline(&self.pipeline);
-        ctx.apply_bindings(&self.bindings);
-
-        for i in 0..10 {
-            let dt = t + std::time::Duration::from_micros(i * 30);
-            let dt = dt - t;
-            let dt = dt.as_millis() as f32;
-
-            ctx.apply_uniforms(&shader::Uniforms {
-                offset: (dt.sin() * 0.5, (dt * 3.0).cos() * 0.5),
-            });
-
-            ctx.draw(0, 6, 1);
-        }
-
-        ctx.end_render_pass();
-        ctx.commit_frame();
-    }
-}
+const WIDTH: usize = 640 / 2;
+const HEIGHT: usize = 360 / 2;
 
 fn main() {
-    start(Default::default(), |mut ctx| {
-        UserData::owning(Stage::new(&mut ctx), ctx)
-    });
-}
+    let mut noise;
+    let mut carry;
+    let mut seed = 0xbeefu32;
 
-mod shader {
-    use miniquad::*;
+    let mut window = Window::new(
+        "Noise Test - Press ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions {
+            resize: true,
+            scale_mode: ScaleMode::UpperLeft,
+            ..WindowOptions::default()
+        },
+    )
+    .expect("Unable to create window");
 
-    pub const VERTEX: &str = r#"#version 100
-    attribute vec2 pos;
-    attribute vec2 uv;
-    uniform vec2 offset;
-    varying lowp vec2 texcoord;
-    void main() {
-        gl_Position = vec4(pos + offset, 0, 1);
-        texcoord = uv;
-    }"#;
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
-    pub const FRAGMENT: &str = r#"#version 100
-    varying lowp vec2 texcoord;
-    uniform sampler2D tex;
-    void main() {
-        gl_FragColor = texture2D(tex, texcoord);
-    }"#;
+    let mut buffer: Vec<u32> = Vec::with_capacity(WIDTH * HEIGHT);
 
-    pub fn meta() -> ShaderMeta {
-        ShaderMeta {
-            images: vec!["tex".to_string()],
-            uniforms: UniformBlockLayout {
-                uniforms: vec![UniformDesc::new("offset", UniformType::Float2)],
-            },
+    let mut size = (0, 0);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        let new_size = (window.get_size().0, window.get_size().1);
+        if new_size != size {
+            size = new_size;
+            buffer.resize(size.0 * size.1, 0);
         }
-    }
 
-    #[repr(C)]
-    pub struct Uniforms {
-        pub offset: (f32, f32),
+        for i in buffer.iter_mut() {
+            noise = seed;
+            noise >>= 3;
+            noise ^= seed;
+            carry = noise & 1;
+            noise >>= 1;
+            seed >>= 1;
+            seed |= carry << 30;
+            noise &= 0xFF;
+            *i = (noise << 16) | (noise << 8) | noise;
+        }
+
+        window.get_keys().map(|keys| {
+            for t in keys {
+                match t {
+                    Key::W => println!("holding w!"),
+                    Key::T => println!("holding t!"),
+                    _ => (),
+                }
+            }
+        });
+
+        window.get_keys_released().map(|keys| {
+            for t in keys {
+                match t {
+                    Key::W => println!("released w!"),
+                    Key::T => println!("released t!"),
+                    _ => (),
+                }
+            }
+        });
+
+        window
+            .update_with_buffer(&buffer, new_size.0, new_size.1)
+            .unwrap();
     }
 }
