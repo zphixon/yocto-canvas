4a06b16db24c48dd21711c863ee5848f4e4aa6b9
Zack <zphixon@gmail.com>
2021-04-04 21:00:08 -0400

    clean up build script

--- build.rs
+++ build.rs
@@ -28,15 +28,8 @@ fn build() -> Result<(), String> {
     let glslang_validator = find_executable(GLSLANG_VALIDATOR)
         .ok_or(format!("Could not find {} on PATH.\nPlease add it to PATH or set environment variable GLSLANG_VALIDATOR_PATH.", GLSLANG_VALIDATOR))?;
 
-    // get shaders dir and cache
+    // get shaders dir
     let shaders_dir = PathBuf::from("shaders");
-    //let mut cache_dir = shaders_dir.clone();
-    //cache_dir.push("cache");
-    //if let Err(e) = create_dir(&cache_dir) {
-    //    if e.kind() != ErrorKind::AlreadyExists {
-    //        return Err(format!("Could not create cache dir: {}", e));
-    //    }
-    //}
 
     let mut output = std::collections::HashSet::new();
 
@@ -47,6 +40,8 @@ fn build() -> Result<(), String> {
     // TODO recursive?
     for entry in dir {
         let entry = entry.map_err(|e| format!("Could not read dir entry\n{}", e))?;
+
+        // only run (the rest of this loop)? if the file is changed
         println!(
             "cargo:rerun-if-changed={}",
             entry.file_name().to_str().unwrap()
@@ -64,74 +59,6 @@ fn build() -> Result<(), String> {
             }
         }
 
-        /*
-        // error out if we already compiled to that name
-        if output.contains(&shader_out) {
-            return Err(format!(
-                "Another shader has already compiled to {}\nPlease rename the shaders.",
-                shader_out.display()
-            ));
-        }
-
-        // read its metadata
-        let meta = entry
-            .metadata()
-            .map_err(|e| format!("Could not read metadata of {}\n{}", shader_in.display(), e))?;
-
-        if meta.is_dir() && shader_in.ends_with("cache") {
-            // skip the cache dir
-            continue;
-        }
-
-        // get last modified date
-        let last_modified = format!(
-            "{:?}",
-            meta.modified().map_err(|e| {
-                format!(
-                    "Could not read modified date of {}\n{}",
-                    shader_in.display(),
-                    e
-                )
-            })?
-        );
-
-        // compare to cached
-        cache_dir.push(shader_in.file_name().unwrap());
-        if let Ok(mut file) = OpenOptions::new().read(true).write(true).open(&cache_dir) {
-            let mut cached_modified = String::new();
-            file.read_to_string(&mut cached_modified).map_err(|e| {
-                format!(
-                    "Could not read cached date of {}\n{}",
-                    shader_in.display(),
-                    e
-                )
-            })?;
-
-            if cached_modified == last_modified {
-                // if they're the same don't bother compiling
-                cache_dir.pop();
-                continue;
-            } else {
-                file.seek(SeekFrom::Start(0)).map_err(|e| {
-                    format!(
-                        "Couldn't seek to beginning of {}\n{}",
-                        cache_dir.display(),
-                        e
-                    )
-                })?;
-                file.write_all(last_modified.as_bytes())
-                    .map_err(|e| format!("Couldn't write {}\n{}", cache_dir.display(), e))?;
-            }
-        } else if let Ok(mut file) = File::create(&cache_dir) {
-            file.write_all(last_modified.as_bytes())
-                .map_err(|e| format!("Couldn't write {}\n{}", cache_dir.display(), e))?;
-        } else {
-            return Err(format!("Could not find or create {}", cache_dir.display()));
-        }
-        cache_dir.pop();
-         */
-
-        // at this point we only have unchanged and unique files left to compile, so remove those spv files
         if let Err(e) = remove_file(&shader_out) {
             if e.kind() != ErrorKind::NotFound {
                 return Err(format!(
@@ -142,12 +69,12 @@ fn build() -> Result<(), String> {
             }
         }
 
-        // and compile!
-        eprintln!(
+        println!(
             "compile {} to {}",
             shader_in.display(),
             shader_out.display()
         );
+
         let out = Command::new(&glslang_validator)
             .arg("-V")
             .arg(&shader_in)
