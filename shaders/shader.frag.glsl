733783e1457f570553a6d80d2fcdad1de8f14a11
Zack H <zphixon@gmail.com>
2021-04-10 17:41:26 -0400

    green

--- shaders/shader.frag.glsl
+++ shaders/shader.frag.glsl
@@ -2,8 +2,8 @@
 
 layout(location=0) out vec4 f_color;
 
-layout(set=0, binding=0) uniform texture2D t_diffuse;
-layout(set=0, binding=1) uniform sampler s_diffuse;
+//layout(set=0, binding=0) uniform texture2D t_diffuse;
+//layout(set=0, binding=1) uniform sampler s_diffuse;
 
 void main() {
     f_color = vec4(0.3, 0.95, 0.12, 1.0);
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 pub use anyhow::{Context, Result};
 
+use bytemuck::{Pod, Zeroable};
+
 use winit::{
     dpi::PhysicalSize,
     event::*,
@@ -8,16 +10,100 @@ use winit::{
 };
 
 use wgpu::{
-    BackendBit, BlendState, ColorTargetState, ColorWrite, CullMode, Device, DeviceDescriptor,
-    Features, FragmentState, FrontFace, Instance, MultisampleState, PipelineLayoutDescriptor,
-    PolygonMode, PresentMode, PrimitiveState, PrimitiveTopology, Queue, RenderPipeline,
-    RenderPipelineDescriptor, RequestAdapterOptions, Surface, SwapChain, SwapChainDescriptor,
-    TextureUsage, VertexState,
+    util::{BufferInitDescriptor, DeviceExt},
+    BackendBit, BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayoutDescriptor,
+    BindGroupLayoutEntry, BindingResource, BindingType, BlendState, Buffer, BufferAddress,
+    BufferUsage, ColorTargetState, ColorWrite, CommandEncoderDescriptor, CullMode, Device,
+    DeviceDescriptor, Features, FragmentState, FrontFace, InputStepMode, Instance, LoadOp,
+    MultisampleState, Operations, PipelineLayoutDescriptor, PolygonMode, PresentMode,
+    PrimitiveState, PrimitiveTopology, Queue, RenderPassColorAttachmentDescriptor,
+    RenderPassDescriptor, RenderPipeline, RenderPipelineDescriptor, RequestAdapterOptions,
+    ShaderStage, Surface, SwapChain, SwapChainDescriptor, SwapChainError, TextureSampleType,
+    TextureUsage, TextureViewDimension, VertexAttribute, VertexBufferLayout, VertexFormat,
+    VertexState,
 };
 
 mod texture;
 
 use texture::MyTexture;
+
+//const vec2 corners[6] = vec2[6](
+//    vec2(-1.0, -1.0),
+//    vec2(1.0, -1.0),
+//    vec2(1.0, 1.0),
+//
+//    vec2(1.0, 1.0),
+//    vec2(-1.0, 1.0),
+//    vec2(-1.0, -1.0)
+//);
+
+//    top left             top right
+// xy -1 -1                1 -1
+// uv  0  1                1  1
+// xy -1  1                1  1
+// uv  0  0                1  0
+//    bottom left          bottom right
+
+const VERTICES: [Vertex; 6] = [
+    // top left
+    Vertex {
+        position: [-1., -1.],
+        tex_coord: [0., 1.],
+    },
+    // top right
+    Vertex {
+        position: [1., -1.],
+        tex_coord: [1., 1.],
+    },
+    // bottom right
+    Vertex {
+        position: [1., 1.],
+        tex_coord: [1., 0.],
+    },
+    // bottom right
+    Vertex {
+        position: [1., 1.],
+        tex_coord: [1., 0.],
+    },
+    // bottom left
+    Vertex {
+        position: [-1., 1.],
+        tex_coord: [0., 0.],
+    },
+    // top left
+    Vertex {
+        position: [-1., -1.],
+        tex_coord: [0., 1.],
+    },
+];
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Pod, Zeroable)]
+struct Vertex {
+    position: [f32; 2],
+    tex_coord: [f32; 2],
+}
+
+impl Vertex {
+    fn desc<'a>() -> VertexBufferLayout<'a> {
+        VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as BufferAddress,
+            step_mode: InputStepMode::Vertex,
+            attributes: &[
+                VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: VertexFormat::Float2,
+                },
+                VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 2]>() as BufferAddress,
+                    shader_location: 1,
+                    format: VertexFormat::Float2,
+                },
+            ],
+        }
+    }
+}
 
 struct State {
     surface: Surface,
@@ -28,6 +114,7 @@ struct State {
     size: PhysicalSize<u32>,
     pipeline: RenderPipeline,
     texture: MyTexture,
+    vertex_buffer: Buffer,
 }
 
 impl State {
@@ -85,7 +172,7 @@ impl State {
             vertex: VertexState {
                 module: &vs_module,
                 entry_point: "main",
-                buffers: &[],
+                buffers: &[Vertex::desc()],
             },
             primitive: PrimitiveState {
                 topology: PrimitiveTopology::TriangleList,
@@ -112,6 +199,12 @@ impl State {
             }),
         });
 
+        let vertex_buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("vertex buffer"),
+            contents: bytemuck::cast_slice(&VERTICES),
+            usage: BufferUsage::VERTEX,
+        });
+
         Ok(Self {
             surface,
             device,
@@ -121,12 +214,56 @@ impl State {
             size,
             pipeline,
             texture,
+            vertex_buffer,
         })
     }
 
     // returns true if state captured the event, false otherwise
     fn input(&mut self, _event: &WindowEvent) -> bool {
         false
+    }
+
+    fn render(&mut self) -> Result<()> {
+        let frame = self.swapchain.get_current_frame()?.output;
+        let mut encoder = self
+            .device
+            .create_command_encoder(&CommandEncoderDescriptor {
+                label: Some("command encoder"),
+            });
+
+        {
+            let mut rp = encoder.begin_render_pass(&RenderPassDescriptor {
+                label: Some("render pass"),
+                color_attachments: &[RenderPassColorAttachmentDescriptor {
+                    attachment: &frame.view,
+                    resolve_target: None,
+                    ops: Operations {
+                        load: LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+
+            rp.set_pipeline(&self.pipeline);
+
+            rp.set_bind_group(0, &self.texture.group, &[]);
+            //rp.set_bind_group(1, self.uniform)
+
+            rp.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+
+            let len = VERTICES.len() as u32;
+            rp.draw(0..len, 0..1);
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+
+        Ok(())
     }
 }
 
@@ -147,6 +284,20 @@ fn main() -> Result<()> {
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
                 _ => {}
             },
+            Event::RedrawRequested(window_id) if window_id == window.id() => {
+                //state.update();
+                match state.render() {
+                    Ok(_) => {}
+                    Err(e) => match e.downcast::<SwapChainError>() {
+                        Ok(e) => match e {
+                            SwapChainError::Lost => {}
+                            SwapChainError::OutOfMemory => *control_flow = ControlFlow::Exit,
+                            e => println!("{}", e),
+                        },
+                        Err(e) => println!("{}", e),
+                    },
+                }
+            }
             _ => {}
         }
     });
