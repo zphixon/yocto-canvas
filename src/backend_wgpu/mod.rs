0a7b34a5bcb0ca85ca07f20471669d8d7db24951
Zack H <zphixon@gmail.com>
2021-04-16 21:43:24 -0400

    move wgpu backend to its own stuff

--- src/backend_wgpu/mod.rs
+++ src/backend_wgpu/mod.rs
@@ -1,8 +1,124 @@
 use bytemuck::{Pod, Zeroable};
 use cgmath::Matrix4;
-use wgpu::{BufferAddress, InputStepMode, VertexAttribute, VertexBufferLayout, VertexFormat};
+use wgpu::{
+    BackendBit, BufferAddress, CommandEncoderDescriptor, Device, DeviceDescriptor, Features,
+    InputStepMode, Instance, PresentMode, Queue, RequestAdapterOptions, Surface, SwapChain,
+    SwapChainDescriptor, TextureUsage, VertexAttribute, VertexBufferLayout, VertexFormat,
+};
 
 pub mod canvas;
+
+use crate::{Context, Result};
+use canvas::CanvasPipeline;
+use winit::dpi::PhysicalSize;
+use winit::window::Window;
+
+pub struct WgpuBackend {
+    pub surface: Surface,
+    pub device: Device,
+    pub queue: Queue,
+    pub swapchain: SwapChain,
+    pub sc_desc: SwapChainDescriptor,
+    pub canvas_pipeline: CanvasPipeline,
+    pub updated_uniforms: bool,
+}
+
+impl WgpuBackend {
+    pub async fn new(window: &Window) -> Result<Self> {
+        let size = window.inner_size();
+        let instance = Instance::new(BackendBit::PRIMARY);
+        let surface = unsafe { instance.create_surface(window) };
+
+        let adapter = instance
+            .request_adapter(&RequestAdapterOptions {
+                power_preference: Default::default(),
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &DeviceDescriptor {
+                    label: Some("device descriptor"),
+                    features: Features::empty(),
+                    limits: Default::default(),
+                },
+                None,
+            )
+            .await
+            .context("Couldn't get device")?;
+
+        let sc_desc = SwapChainDescriptor {
+            usage: TextureUsage::RENDER_ATTACHMENT | TextureUsage::COPY_SRC,
+            format: adapter.get_swap_chain_preferred_format(&surface),
+            width: size.width,
+            height: size.height,
+            present_mode: PresentMode::Fifo,
+        };
+
+        let swapchain = device.create_swap_chain(&surface, &sc_desc);
+
+        let canvas_pipeline = CanvasPipeline::new(&device, &queue, &sc_desc)?;
+
+        Ok(WgpuBackend {
+            surface,
+            device,
+            queue,
+            swapchain,
+            sc_desc,
+            canvas_pipeline,
+            updated_uniforms: false,
+        })
+    }
+
+    pub fn update(&mut self, size: &PhysicalSize<u32>, zoom: f32) {
+        if !self.updated_uniforms {
+            let uniform = Uniform {
+                scale_x: self.canvas_pipeline.canvas_image.width() as f32 / size.width as f32,
+                scale_y: self.canvas_pipeline.canvas_image.height() as f32 / size.height as f32,
+                xform_x: 0.0,
+                xform_y: 0.0,
+                zoom,
+            };
+
+            self.queue.write_buffer(
+                &self.canvas_pipeline.canvas_uniform_buffer,
+                0,
+                bytemuck::cast_slice(&[uniform]),
+            );
+            self.updated_uniforms = true;
+        }
+    }
+
+    pub fn resize(&mut self, new_size: PhysicalSize<u32>) {
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swapchain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    pub fn render(&mut self, size: &PhysicalSize<u32>) -> Result<()> {
+        let frame = self.swapchain.get_current_frame()?.output;
+        let mut encoder = self
+            .device
+            .create_command_encoder(&CommandEncoderDescriptor {
+                label: Some("command encoder"),
+            });
+
+        self.canvas_pipeline.execute(
+            &mut encoder,
+            &self.queue,
+            &frame,
+            size.width as f32,
+            size.height as f32,
+        );
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        self.updated_uniforms = false;
+
+        Ok(())
+    }
+}
 
 #[rustfmt::skip]
 #[allow(dead_code)]
--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,15 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
-use wgpu::{
-    BackendBit, CommandEncoderDescriptor, Device, DeviceDescriptor, Features, Instance,
-    PresentMode, Queue, RequestAdapterOptions, Surface, SwapChain, SwapChainDescriptor,
-    SwapChainError, TextureUsage,
-};
+use wgpu::SwapChainError;
 
 mod backend_wgpu;
 mod composite;
 mod image;
 mod texture;
 
-use crate::backend_wgpu::Uniform;
-use crate::{backend_wgpu::canvas::CanvasPipeline, image::Pixel};
+use crate::backend_wgpu::WgpuBackend;
+use crate::image::Pixel;
 
 #[derive(Debug)]
 struct Mouse {
@@ -31,55 +27,16 @@ struct Mouse {
 
 #[allow(dead_code)]
 struct State {
-    surface: Surface,
-    device: Device,
-    queue: Queue,
-    swapchain: SwapChain,
-    sc_desc: SwapChainDescriptor,
     size: PhysicalSize<u32>,
-    canvas_pipeline: CanvasPipeline,
     mouse: Mouse,
     zoom: f32,
-    updated_uniforms: bool,
+    wgpu_backend: Option<WgpuBackend>,
+    cpu_backend: Option<()>,
 }
 
 impl State {
     async fn new(window: &Window) -> Result<Self> {
         let size = window.inner_size();
-        let instance = Instance::new(BackendBit::PRIMARY);
-        let surface = unsafe { instance.create_surface(window) };
-
-        let adapter = instance
-            .request_adapter(&RequestAdapterOptions {
-                power_preference: Default::default(),
-                compatible_surface: Some(&surface),
-            })
-            .await
-            .unwrap();
-
-        let (device, queue) = adapter
-            .request_device(
-                &DeviceDescriptor {
-                    label: Some("device descriptor"),
-                    features: Features::empty(),
-                    limits: Default::default(),
-                },
-                None,
-            )
-            .await
-            .context("Couldn't get device")?;
-
-        let sc_desc = SwapChainDescriptor {
-            usage: TextureUsage::RENDER_ATTACHMENT | TextureUsage::COPY_SRC,
-            format: adapter.get_swap_chain_preferred_format(&surface),
-            width: size.width,
-            height: size.height,
-            present_mode: PresentMode::Fifo,
-        };
-
-        let swapchain = device.create_swap_chain(&surface, &sc_desc);
-
-        let canvas_pipeline = CanvasPipeline::new(&device, &queue, &sc_desc)?;
 
         let mouse = Mouse {
             x: size.width as f32 / 2.,
@@ -90,19 +47,14 @@ impl State {
 
         let zoom = 1.0;
 
-        let updated_uniforms = false;
+        let wgpu_backend = Some(WgpuBackend::new(window).await?);
 
         Ok(Self {
-            surface,
-            device,
-            queue,
-            swapchain,
-            sc_desc,
             size,
-            canvas_pipeline,
             mouse,
             zoom,
-            updated_uniforms,
+            wgpu_backend,
+            cpu_backend: None,
         })
     }
 
@@ -137,74 +89,46 @@ impl State {
     }
 
     fn update(&mut self) {
-        if self.mouse.left == ElementState::Pressed {
-            self.canvas_pipeline.canvas_image.set_pixel(
-                40,
-                20,
-                Pixel {
-                    r: 1.0,
-                    g: 1.0,
-                    b: 1.0,
-                    a: 1.0,
-                },
-            );
-        } else {
-            self.canvas_pipeline.canvas_image.set_pixel(
-                40,
-                20,
-                Pixel {
-                    r: 1.0,
-                    g: 0.0,
-                    b: 0.0,
-                    a: 1.0,
-                },
-            );
-        }
+        if let Some(wgpu_backend) = &mut self.wgpu_backend {
+            if self.mouse.left == ElementState::Pressed {
+                wgpu_backend.canvas_pipeline.canvas_image.set_pixel(
+                    40,
+                    20,
+                    Pixel {
+                        r: 1.0,
+                        g: 1.0,
+                        b: 1.0,
+                        a: 1.0,
+                    },
+                );
+            } else {
+                wgpu_backend.canvas_pipeline.canvas_image.set_pixel(
+                    40,
+                    20,
+                    Pixel {
+                        r: 1.0,
+                        g: 0.0,
+                        b: 0.0,
+                        a: 1.0,
+                    },
+                );
+            }
 
-        if !self.updated_uniforms {
-            let uniform = Uniform {
-                scale_x: self.canvas_pipeline.canvas_image.width() as f32 / self.size.width as f32,
-                scale_y: self.canvas_pipeline.canvas_image.height() as f32
-                    / self.size.height as f32,
-                xform_x: 0.0,
-                xform_y: 0.0,
-                zoom: self.zoom,
-            };
-
-            self.queue.write_buffer(
-                &self.canvas_pipeline.canvas_uniform_buffer,
-                0,
-                bytemuck::cast_slice(&[uniform]),
-            );
-            self.updated_uniforms = true;
+            wgpu_backend.update(&self.size, self.zoom);
         }
     }
 
     fn resize(&mut self, new_size: PhysicalSize<u32>) {
         self.size = new_size;
-        self.sc_desc.width = new_size.width;
-        self.sc_desc.height = new_size.height;
-        self.swapchain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+        if let Some(wgpu_backend) = &mut self.wgpu_backend {
+            wgpu_backend.resize(new_size);
+        }
     }
 
     fn render(&mut self) -> Result<()> {
-        let frame = self.swapchain.get_current_frame()?.output;
-        let mut encoder = self
-            .device
-            .create_command_encoder(&CommandEncoderDescriptor {
-                label: Some("command encoder"),
-            });
-
-        self.canvas_pipeline.execute(
-            &mut encoder,
-            &self.queue,
-            &frame,
-            self.size.width as f32,
-            self.size.height as f32,
-        );
-
-        self.queue.submit(std::iter::once(encoder.finish()));
-        self.updated_uniforms = false;
+        if let Some(wgpu_backend) = &mut self.wgpu_backend {
+            wgpu_backend.render(&self.size)?;
+        }
 
         Ok(())
     }
